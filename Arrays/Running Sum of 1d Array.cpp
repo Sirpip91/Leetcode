39c4ec69afed391f6111e90147e3c2488090e979
Bradley Allen <allenbradley91@hotmail.com>
2022-07-01 16:16:18 -0500

    Update Running Sum of 1d Array.cpp

--- Arrays/Running Sum of 1d Array.cpp
+++ Arrays/Running Sum of 1d Array.cpp
@@ -1,1 +1,23 @@
 
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        
+        vector<int> v;
+        
+        
+        int n = nums.size();
+        int sum = 0;
+    
+        
+        for(int i =0; i<n; i++)
+        {
+            sum = sum +nums[i];
+            
+            v.push_back(sum);
+        
+        }
+        
+        return v;
+    }
+};
