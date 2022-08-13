7fec188f7bd150bc59329b9c60dd3d8370156562
Bradley Allen <allenbradley91@hotmail.com>
2022-08-13 15:58:52 -0500

    Update AddTwoNumbers.cpp

--- LinkedList/AddTwoNumbers.cpp
+++ LinkedList/AddTwoNumbers.cpp
@@ -10,34 +10,59 @@
  */
 class Solution {
 public:
+
+    //MEDIUM PROBLEM SOLUTION: https://www.youtube.com/watch?v=di_jUp3tWUI&ab_channel=CodewithAlisha
+
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-        // create a answer list node
-        ListNode *dummy = new ListNode(0);
-        ListNode *temp = dummy;
-        
-        
+        ListNode* l3 = new ListNode(0);
+        ListNode* head = l3;
         int carry = 0;
+        while(l1 && l2)
+        {
+            int value  = l1 ->val + l2->val + carry;
+            carry = value / 10 ; //  16/10 =  1
+            l3->next = new ListNode(value%10); // insert 6
+            l3 = l3->next;
+            l1=l1->next;
+            l2=l2->next;
+        }
         
-        while(l1 != NULL || l2!=NULL || carry){
-            int sum = 0;
-            
-            if(l1!=NULL){
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            
-            if(l2!=NULL){
-                sum += l2->val;
-                l2 = l2->next;
-            }
-            sum+=carry;
-            carry =sum/10;
-            ListNode *node = new ListNode(sum%10);
-            temp->next= node;
-            temp= temp->next;
-        }
-        return dummy->next;
-        }
+        //check fi l1>l2 or l2<l1
+        
+         while(l1)
+        {
+            int value  = l1 ->val + carry;
+            carry = value / 10 ; //  16/10 =  1
+            l3->next = new ListNode(value%10); // insert 6
+            l3 = l3->next;
+            l1=l1->next;
     
+        }
+        
+         while(l2)
+        {
+            int value  =  l2->val + carry;
+            carry = value / 10 ; //  16/10 =  1
+            l3->next = new ListNode(value%10); // insert 6
+            l3 = l3->next;
+          
+            l2=l2->next;
+        }
+        
+        //if we have a carry over at the end 
+        if(carry)
+        {
+            l3->next = new ListNode(carry);
+        }
+        
+        
+        
+        //l3 will be pointing last rn 
+        //but we have a head pointer saved
+        
+        return head->next;
+        
+        
+    }
 };
