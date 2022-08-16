4eb9e20201ecebb4003e03ea82b2904cb3d57574
Bradley Allen <allenbradley91@hotmail.com>
2022-08-16 13:49:36 -0500

    Create 5. Longest Palindromic Substring.cpp

---
+++ Vprep/5. Longest Palindromic Substring.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+       string longestPalindrome(string s) {
+        int n = s.size();
+        string ans = "";
+        int max_l = 0;
+        for(int i = 0; i < n; i++){
+            int l = i-1;
+            int r = i+1;
+            while(l >= 0 && s[i] == s[l])
+                l--;     //check for duplicates and skip the index
+            while(r < n && s[i] == s[r])
+                r++;  //check for duplicates and skip the index
+            while(l >= 0 && r < n && s[l] == s[r]){
+                l--;
+                r++;
+            }
+            if(r-l-1 > max_l){
+                max_l = r-l-1;
+                ans = s.substr(l+1, r-l-1);
+            }
+        }
+        return ans;
+    }
+};
