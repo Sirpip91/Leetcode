0150c6b0dbb8bdf7b21beb7eb97651f39e2661e3
Bradley Allen <allenbradley91@hotmail.com>
2022-08-18 11:32:19 -0500

    Create 121. Best Time to Buy and Sell Stock.cpp

---
+++ Vprep/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        //O(N) time comlpexity
+        //O(1) space comleixty
+            if(prices.size()<2)return 0;
+            int min = prices[0];
+            int profit = 0;
+        
+        for(int i = 1;i<prices.size();i++)
+        {
+            //ceck if profit is greater than cur profit
+            if(prices[i] - min > profit)//update profit.
+                profit = prices[i] - min;
+           //update the min
+            if(prices[i] < min)
+                min = prices[i];
+        }
+        
+        return profit;
+            
+    }
+};
