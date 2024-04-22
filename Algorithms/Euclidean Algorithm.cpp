e5840c66efe19d3ae5ad919b74d19e6e3921230f
Bradley Allen <allenbradley91@hotmail.com>
2024-04-22 14:53:53 -0600

    Create Euclidean Algorithm.cpp

---
+++ Algorithms/Euclidean Algorithm.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//finding the GCD of two values.
+int main()
+{
+	
+	//Time complexity Olog5(min(a,b))
+	int a = 0;
+	int b = 0;
+
+	cin >> a >> b;
+	//while they are not 0
+	while(a > 0 && b > 0)
+	{
+		//get the larger value and divide (get remainder of the other)
+		if(a>b)
+			a = a % b;
+		else
+			b = b % a;
+		
+	}
+		//check which value is a 0 and you now have the GCD
+		if(a == 0)
+			cout << b;
+		else
+			cout << a;
+    return 0;
+
+}
